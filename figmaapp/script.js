31d20c60ef1f0180186ca0c8fdec964e2bbd3cc7
Lucas de Souza <73202847+lucas-esp@users.noreply.github.com>
2022-07-26 16:27:06 -0300

    Add files via upload

---
+++ figmaapp/script.js
@@ -0,0 +1,10 @@
+function redirecionar(){
+    window.location.href = "index.html";
+}
+
+function redirecionar2(){
+    window.location.href = "index2.html";
+}
+function redirecionar3(){
+    window.location.href = "index3.html";
+}
